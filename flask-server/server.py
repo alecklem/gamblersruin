17958e8cc8f1bb4812b56c82b52e6940df08968f
alecklem <alecklem@tamu.edu>
2023-12-12 10:59:43 -0600

    still cors error, fixed loadplayers endpoint and added default endpoint

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,31 +1,19 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
-from nba_api.stats.endpoints import commonallplayers
+from nba_api.stats.static import players
 
 app = Flask(__name__)
 CORS(app) 
 
+@app.route('/')
+def default():
+    return "hello"
+
+
 @app.route('/loadplayers', methods=['GET'])
 def load_players():
     try:
-        # Use the nba_api to get all current players
-        players_endpoint = commonallplayers.CommonAllPlayers(is_only_current_season=1)
-        players_data = players_endpoint.get_json()
-
-        # Extract relevant player information (you may need to adapt this based on the API response structure)
-        players = []
-        for player in players_data['resultSets'][0]['rowSet']:
-            player_info = {
-                'id': player['PERSON_ID'],  # Adjust the index based on the actual structure
-                'full_name': f"{player['DISPLAY_LAST_COMMA_FIRST']}",
-                'team': player['TEAM_ABBREVIATION'],
-            }
-            players.append(player_info)
-
-
-
-        # Return the player data in the response
-        return jsonify({'players': players})
+        return players.get_active_players()
 
     except Exception as e:
         return jsonify({'error': str(e)}), 500
