01b6a30130ec25a9ef51edd3744126c3a7eb1b84
alecklem <alecklem@tamu.edu>
2023-12-13 00:30:53 -0600

    routes for all categories, got button and submit query working

--- client/src/PlayerList.js
+++ client/src/PlayerList.js
@@ -5,6 +5,25 @@ const PlayerList = () => {
   const [players, setPlayers] = useState([]);
   const [searchQuery, setSearchQuery] = useState("");
   const [filteredPlayers, setFilteredPlayers] = useState([]);
+  const [selectedPlayer, setSelectedPlayer] = useState(null);
+  const [selectedStat, setSelectedStat] = useState("POINTS");
+
+  const statsOptions = [
+    "POINTS",
+    "REBOUNDS",
+    "ASSISTS",
+    "3PT",
+    "3PTA",
+    "FG",
+    "FGA",
+    "STEALS",
+    "BLOCKS",
+    "STOCKS",
+    "PRA",
+    "PTS + REBOUNDS",
+    "POINTS + ASSISTS",
+    "TURNOVERS",
+  ];
 
   useEffect(() => {
     const fetchPlayers = async () => {
@@ -20,34 +39,132 @@ const PlayerList = () => {
   }, []);
 
   useEffect(() => {
-    // Filter players based on the search query
+    // Filter players based on the search query and selected stat
     const filtered = players.filter((player) =>
       player.full_name.toLowerCase().includes(searchQuery.toLowerCase())
     );
+
     setFilteredPlayers(filtered);
-  }, [searchQuery, players]);
+  }, [searchQuery, players, selectedStat]);
+
+  const routeMappings = {
+    "POINTS": "points",
+    "REBOUNDS": "rebounds",
+    "ASSISTS": "assists",
+    "3PT": "three_pointers",
+    "3PTA": "three_point_attempts",
+    "FG": "field_goals",
+    "FGA": "field_goals_attempted",
+    "STEALS": "steals",
+    "BLOCKS": "blocks",
+    "STOCKS": "stocks",
+    "PRA": "pra",
+    "PTS + REBOUNDS": "points_plus_rebounds",
+    "POINTS + ASSISTS": "points_plus_assists",
+    "TURNOVERS": "turnovers",
+  };
+  
+  const handleSave = async () => {
+    if (selectedPlayer) {
+      try {
+        const route = `http://127.0.0.1:5000/${routeMappings[selectedStat]}`;
+        const response = await axios.post(route, {
+          player: selectedPlayer,
+          category: selectedStat,
+        });
+  
+        // Handle the response if needed
+        console.log(response.data);
+      } catch (error) {
+        console.error("Error saving data:", error);
+      }
+    }
+  };
+
+  const handlePlayerClick = (player) => {
+    // Toggle the selected player
+    setSelectedPlayer((prevSelected) => {
+      if (prevSelected && prevSelected.id === player.id) {
+        // Unselect the player if already selected
+        return null;
+      } else {
+        // Select the clicked player
+        return player;
+      }
+    });
+
+    // Reset the search query when unselecting a player
+    setSearchQuery("");
+  };
+
+  const [isDropdownVisible, setIsDropdownVisible] = useState(false);
+
+  const handleStatSelect = (stat) => {
+    setSelectedStat(stat);
+    setIsDropdownVisible(false); // Hide the dropdown after selecting a stat
+  };
 
   return (
     <div>
       <h1>Current NBA Players</h1>
       <div className="search-container">
-        <input
-          type="text"
-          placeholder="Search for a player"
-          value={searchQuery}
-          onChange={(e) => setSearchQuery(e.target.value)}
-        />
+        <div>
+          {!selectedPlayer && (
+            <input
+              type="text"
+              placeholder="Search for a player"
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+            />
+          )}
+
+          {/* Button for stat selection */}
+          <div className="stat-dropdown">
+            <button className="stat-button" onClick={() => setIsDropdownVisible(!isDropdownVisible)}>
+              {selectedStat} {/* Display the selected stat in the button */}
+            </button>
+            {isDropdownVisible && (
+              <ul className="stat-options">
+                {statsOptions.map((stat) => (
+                  <li key={stat} onClick={() => handleStatSelect(stat)}>
+                    {stat}
+                  </li>
+                ))}
+              </ul>
+            )}
+          </div>
+          <button onClick={handleSave}>Save Selected Player</button>
+        </div>
+
         <ul className="dropdown">
-          {filteredPlayers.map((player) => (
-            <li key={player.id}>
+          {selectedPlayer ? (
+            <li 
+            key={selectedPlayer.id}
+            onClick={() => handlePlayerClick(selectedPlayer)}
+            >
               <img
                 width={60}
-                src={`https://cdn.nba.com/headshots/nba/latest/1040x760/${player.id}.png`}
-                alt={`${player.full_name}'s headshot`}
+                src={`https://cdn.nba.com/headshots/nba/latest/1040x760/${selectedPlayer.id}.png`}
+                alt={`${selectedPlayer.full_name}'s headshot`}
               />
-              {player.full_name}
+              {selectedPlayer.full_name}
             </li>
-          ))}
+          ) : (
+            filteredPlayers.map((player) => (
+              <li
+                key={player.id}
+                onClick={() => handlePlayerClick(player)}
+                className={selectedPlayer && selectedPlayer.id === player.id ? "selected" : ""}
+              >
+                <img
+                  width={60}
+                  src={`https://cdn.nba.com/headshots/nba/latest/1040x760/${player.id}.png`}
+                  alt={`${player.full_name}'s headshot`}
+                />
+                {player.full_name}
+              </li>
+            ))
+          )}
         </ul>
       </div>
     </div>
--- flask-server/server.py
+++ flask-server/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 from nba_api.stats.static import players
 
@@ -17,6 +17,259 @@ def load_players():
 
     except Exception as e:
         return jsonify({'error': str(e)}), 500
+    
+@app.route('/points', methods=['POST'])
+def points():
+    try:
+        data = request.get_json()
+        player = data.get('player')
+        category = data.get('category')
+
+        # Access the player and category data as needed
+        player_full_name = player.get('full_name')
+
+        # Perform actions based on player and category data
+        result = f"Player: {player_full_name}, Category: {category}"
+        print(result)
+        return jsonify({'result': result})
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+    
+@app.route('/rebounds', methods=['POST'])
+def rebounds():
+    try:
+        data = request.get_json()
+        player = data.get('player')
+        category = data.get('category')
+
+        # Access the player and category data as needed
+        player_full_name = player.get('full_name')
+
+        # Perform actions based on player and category data
+        result = f"Player: {player_full_name}, Category: {category}"
+        return jsonify({'result': result})
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+# Route for handling the 'ASSISTS' category
+@app.route('/assists', methods=['POST'])
+def assists():
+    try:
+        data = request.get_json()
+        player = data.get('player')
+        category = data.get('category')
+
+        # Access the player and category data as needed
+        player_full_name = player.get('full_name')
+
+        # Perform actions based on player and category data
+        result = f"Player: {player_full_name}, Category: {category}"
+        return jsonify({'result': result})
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+# Route for handling the '3PT' category
+@app.route('/three_pointers', methods=['POST'])
+def three_pointers():
+    try:
+        data = request.get_json()
+        player = data.get('player')
+        category = data.get('category')
+
+        # Access the player and category data as needed
+        player_full_name = player.get('full_name')
+
+        # Perform actions based on player and category data
+        result = f"Player: {player_full_name}, Category: {category}"
+        return jsonify({'result': result})
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+@app.route('/three_point_attempts', methods=['POST'])
+def three_point_attempts():
+    try:
+        data = request.get_json()
+        player = data.get('player')
+        category = data.get('category')
+
+        # Access the player and category data as needed
+        player_full_name = player.get('full_name')
+
+        # Perform actions based on player and category data
+        result = f"Player: {player_full_name}, Category: {category}"
+        return jsonify({'result': result})
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+# Route for handling the 'FG' category
+@app.route('/field_goals', methods=['POST'])
+def field_goals():
+    try:
+        data = request.get_json()
+        player = data.get('player')
+        category = data.get('category')
+
+        # Access the player and category data as needed
+        player_full_name = player.get('full_name')
+
+        # Perform actions based on player and category data
+        result = f"Player: {player_full_name}, Category: {category}"
+        return jsonify({'result': result})
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+
+@app.route('/field_goals_attempted', methods=['POST'])
+def field_goals_attempted():
+    try:
+        data = request.get_json()
+        player = data.get('player')
+        category = data.get('category')
+
+        # Access the player and category data as needed
+        player_full_name = player.get('full_name')
+
+        # Perform actions based on player and category data
+        result = f"Player: {player_full_name}, Category: {category}"
+        return jsonify({'result': result})
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+# Route for handling the 'STEALS' category
+@app.route('/steals', methods=['POST'])
+def steals():
+    try:
+        data = request.get_json()
+        player = data.get('player')
+        category = data.get('category')
+
+        # Access the player and category data as needed
+        player_full_name = player.get('full_name')
+
+        # Perform actions based on player and category data
+        result = f"Player: {player_full_name}, Category: {category}"
+        return jsonify({'result': result})
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+# Route for handling the 'BLOCKS' category
+@app.route('/blocks', methods=['POST'])
+def blocks():
+    try:
+        data = request.get_json()
+        player = data.get('player')
+        category = data.get('category')
+
+        # Access the player and category data as needed
+        player_full_name = player.get('full_name')
+
+        # Perform actions based on player and category data
+        result = f"Player: {player_full_name}, Category: {category}"
+        return jsonify({'result': result})
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+# Route for handling the 'STOCKS' category
+@app.route('/stocks', methods=['POST'])
+def stocks():
+    try:
+        data = request.get_json()
+        player = data.get('player')
+        category = data.get('category')
+
+        # Access the player and category data as needed
+        player_full_name = player.get('full_name')
+
+        # Perform actions based on player and category data
+        result = f"Player: {player_full_name}, Category: {category}"
+        return jsonify({'result': result})
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+
+# Route for handling the 'PRA' category
+@app.route('/pra', methods=['POST'])
+def pra():
+    try:
+        data = request.get_json()
+        player = data.get('player')
+        category = data.get('category')
+
+        # Access the player and category data as needed
+        player_full_name = player.get('full_name')
+
+        # Perform actions based on player and category data
+        result = f"Player: {player_full_name}, Category: {category}"
+        return jsonify({'result': result})
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+# Route for handling the 'PTS + REBOUNDS' category
+@app.route('/points_plus_rebounds', methods=['POST'])
+def points_plus_rebounds():
+    try:
+        data = request.get_json()
+        player = data.get('player')
+        category = data.get('category')
+
+        # Access the player and category data as needed
+        player_full_name = player.get('full_name')
+
+        # Perform actions based on player and category data
+        result = f"Player: {player_full_name}, Category: {category}"
+        return jsonify({'result': result})
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+# Route for handling the 'POINTS + ASSISTS' category
+@app.route('/points_plus_assists', methods=['POST'])
+def points_plus_assists():
+    try:
+        data = request.get_json()
+        player = data.get('player')
+        category = data.get('category')
+
+        # Access the player and category data as needed
+        player_full_name = player.get('full_name')
+
+        # Perform actions based on player and category data
+        result = f"Player: {player_full_name}, Category: {category}"
+        return jsonify({'result': result})
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+# Route for handling the 'TURNOVERS' category
+@app.route('/turnovers', methods=['POST'])
+def turnovers():
+    try:
+        data = request.get_json()
+        player = data.get('player')
+        category = data.get('category')
+
+        # Access the player and category data as needed
+        player_full_name = player.get('full_name')
+
+        # Perform actions based on player and category data
+        result = f"Player: {player_full_name}, Category: {category}"
+        return jsonify({'result': result})
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
