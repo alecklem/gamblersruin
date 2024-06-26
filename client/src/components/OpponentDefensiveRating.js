8dca0c37d24e5ed99173de2875d0141cfe0374f5
Alec Klem <alecklem@tamu.edu>
2024-06-26 14:41:18 -0400

    updated metrics

--- client/src/components/OpponentDefensiveRating.js
+++ client/src/components/OpponentDefensiveRating.js
@@ -1,73 +1,78 @@
-import React, { useState } from "react";
+import React from "react";
 
 const OpponentDefensiveRating = ({ metrics }) => {
-  const [showAdvanced, setShowAdvanced] = useState(false);
-
   const teamMetricsLabels = [
-    { label: "Defensive Rating", value: metrics["E_DEF_RATING"] },
-    { label: "Offensive Rating", value: metrics["E_OFF_RATING"] },
-    { label: "Net Rating", value: metrics["E_NET_RATING"] },
-    { label: "Assist Ratio", value: metrics["E_AST_RATIO"] },
-    { label: "Offensive Rebound Percentage", value: metrics["E_OREB_PCT"] },
-    { label: "Defensive Rebound Percentage", value: metrics["E_DREB_PCT"] },
-    { label: "Rebound Percentage", value: metrics["E_REB_PCT"] },
-    { label: "Turnover Percentage", value: metrics["E_TOV_PCT"] },
-    { label: "Pace", value: metrics["E_PACE"] },
+    {
+      label: "Defensive Rating",
+      value: metrics["E_DEF_RATING"],
+      rank: metrics["E_DEF_RATING_RANK"],
+    },
+    {
+      label: "Offensive Rating",
+      value: metrics["E_OFF_RATING"],
+      rank: metrics["E_OFF_RATING_RANK"],
+    },
+    {
+      label: "Net Rating",
+      value: metrics["E_NET_RATING"],
+      rank: metrics["E_NET_RATING_RANK"],
+    },
+    {
+      label: "Assist Ratio",
+      value: metrics["E_AST_RATIO"],
+      rank: metrics["E_AST_RATIO_RANK"],
+    },
+    {
+      label: "Offensive Rebound Percentage",
+      value: metrics["E_OREB_PCT"],
+      rank: metrics["E_OREB_PCT_RANK"],
+    },
+    {
+      label: "Defensive Rebound Percentage",
+      value: metrics["E_DREB_PCT"],
+      rank: metrics["E_DREB_PCT_RANK"],
+    },
+    {
+      label: "Rebound Percentage",
+      value: metrics["E_REB_PCT"],
+      rank: metrics["E_REB_PCT_RANK"],
+    },
+    {
+      label: "Turnover Percentage",
+      value: metrics["E_TOV_PCT"],
+      rank: metrics["E_TOV_PCT_RANK"],
+    },
+    { label: "Pace", value: metrics["E_PACE"], rank: metrics["E_PACE_RANK"] },
+    { label: "Minutes", value: metrics["MIN"], rank: metrics["MIN_RANK"] },
+    {
+      label: "Win Percentage",
+      value: metrics["W_PCT"],
+      rank: metrics["W_PCT_RANK"],
+    },
+    { label: "Games Played", value: metrics["GP"], rank: metrics["GP_RANK"] },
+    { label: "Wins", value: metrics["W"], rank: metrics["W_RANK"] },
+    { label: "Losses", value: metrics["L"], rank: metrics["L_RANK"] },
   ];
 
   const advancedMetricsLabels = [
     { label: "Team Name", value: metrics["TEAM_NAME"] },
-    { label: "Games Played", value: metrics["GP"] },
-    { label: "Wins", value: metrics["W"] },
-    { label: "Losses", value: metrics["L"] },
-    { label: "Win Percentage", value: metrics["W_PCT"] },
-    { label: "Minutes", value: metrics["MIN"] },
-    { label: "Games Played Rank", value: metrics["GP_RANK"] },
-    { label: "Wins Rank", value: metrics["W_RANK"] },
-    { label: "Losses Rank", value: metrics["L_RANK"] },
-    { label: "Win Percentage Rank", value: metrics["W_PCT_RANK"] },
-    { label: "Minutes Rank", value: metrics["MIN_RANK"] },
-    { label: "Offensive Rating Rank", value: metrics["E_OFF_RATING_RANK"] },
-    { label: "Defensive Rating Rank", value: metrics["E_DEF_RATING_RANK"] },
-    { label: "Net Rating Rank", value: metrics["E_NET_RATING_RANK"] },
-    { label: "Assist Ratio Rank", value: metrics["E_AST_RATIO_RANK"] },
-    {
-      label: "Offensive Rebound Percentage Rank",
-      value: metrics["E_OREB_PCT_RANK"],
-    },
-    {
-      label: "Defensive Rebound Percentage Rank",
-      value: metrics["E_DREB_PCT_RANK"],
-    },
-    { label: "Rebound Percentage Rank", value: metrics["E_REB_PCT_RANK"] },
-    { label: "Turnover Percentage Rank", value: metrics["E_TOV_PCT_RANK"] },
-    { label: "Pace Rank", value: metrics["E_PACE_RANK"] },
   ];
 
   return (
     <div>
       <ul>
-        {teamMetricsLabels.map((metric, index) => (
+        {advancedMetricsLabels.map((metric, index) => (
           <li key={index}>
             <strong>{metric.label}:</strong> {metric.value}
           </li>
         ))}
+        {teamMetricsLabels.map((metric, index) => (
+          <li key={index}>
+            <strong>{metric.label}:</strong> {metric.value}{" "}
+            {metric.rank && `[#${metric.rank}]`}
+          </li>
+        ))}
       </ul>
-      <button
-        onClick={() => setShowAdvanced(!showAdvanced)}
-        className="bg-blue-500 text-white px-4 py-2 rounded mt-4"
-      >
-        {showAdvanced ? "Hide Advanced Stats" : "Show Advanced Stats"}
-      </button>
-      {showAdvanced && (
-        <ul className="mt-4">
-          {advancedMetricsLabels.map((metric, index) => (
-            <li key={index}>
-              <strong>{metric.label}:</strong> {metric.value}
-            </li>
-          ))}
-        </ul>
-      )}
     </div>
   );
 };
--- client/src/components/PlayerEstimatedMetrics.js
+++ client/src/components/PlayerEstimatedMetrics.js
@@ -1,48 +1,62 @@
 import React, { useState } from "react";
 
 const PlayerEstimatedMetrics = ({ metrics }) => {
-  const [showAdvanced, setShowAdvanced] = useState(false);
-
   const playerMetricsLabels = [
-    { label: "Minutes", value: metrics["MIN"] },
-    { label: "Offensive Rating", value: metrics["E_OFF_RATING"] },
-    { label: "Defensive Rating", value: metrics["E_DEF_RATING"] },
-    { label: "Net Rating", value: metrics["E_NET_RATING"] },
-    { label: "Assist Ratio", value: metrics["E_AST_RATIO"] },
-    { label: "Offensive Rebound Percentage", value: metrics["E_OREB_PCT"] },
-    { label: "Defensive Rebound Percentage", value: metrics["E_DREB_PCT"] },
-    { label: "Rebound Percentage", value: metrics["E_REB_PCT"] },
-    { label: "Turnover Percentage", value: metrics["E_TOV_PCT"] },
-    { label: "Usage Percentage", value: metrics["E_USG_PCT"] },
-    { label: "Pace", value: metrics["E_PACE"] },
-  ];
-
-  const advancedMetricsLabels = [
-    { label: "Games Played", value: metrics["GP"] },
-    { label: "Wins", value: metrics["W"] },
-    { label: "Losses", value: metrics["L"] },
-    { label: "Win Percentage", value: metrics["W_PCT"] },
-    { label: "Games Played Rank", value: metrics["GP_RANK"] },
-    { label: "Wins Rank", value: metrics["W_RANK"] },
-    { label: "Losses Rank", value: metrics["L_RANK"] },
-    { label: "Win Percentage Rank", value: metrics["W_PCT_RANK"] },
-    { label: "Minutes Rank", value: metrics["MIN_RANK"] },
-    { label: "Offensive Rating Rank", value: metrics["E_OFF_RATING_RANK"] },
-    { label: "Defensive Rating Rank", value: metrics["E_DEF_RATING_RANK"] },
-    { label: "Net Rating Rank", value: metrics["E_NET_RATING_RANK"] },
-    { label: "Assist Ratio Rank", value: metrics["E_AST_RATIO_RANK"] },
+    { label: "Minutes", value: metrics["MIN"], rank: metrics["MIN_RANK"] },
     {
-      label: "Offensive Rebound Percentage Rank",
-      value: metrics["E_OREB_PCT_RANK"],
+      label: "Offensive Rating",
+      value: metrics["E_OFF_RATING"],
+      rank: metrics["E_OFF_RATING_RANK"],
     },
     {
-      label: "Defensive Rebound Percentage Rank",
-      value: metrics["E_DREB_PCT_RANK"],
+      label: "Defensive Rating",
+      value: metrics["E_DEF_RATING"],
+      rank: metrics["E_DEF_RATING_RANK"],
     },
-    { label: "Rebound Percentage Rank", value: metrics["E_REB_PCT_RANK"] },
-    { label: "Turnover Percentage Rank", value: metrics["E_TOV_PCT_RANK"] },
-    { label: "Usage Percentage Rank", value: metrics["E_USG_PCT_RANK"] },
-    { label: "Pace Rank", value: metrics["E_PACE_RANK"] },
+    {
+      label: "Net Rating",
+      value: metrics["E_NET_RATING"],
+      rank: metrics["E_NET_RATING_RANK"],
+    },
+    {
+      label: "Assist Ratio",
+      value: metrics["E_AST_RATIO"],
+      rank: metrics["E_AST_RATIO_RANK"],
+    },
+    {
+      label: "Offensive Rebound Percentage",
+      value: metrics["E_OREB_PCT"],
+      rank: metrics["E_OREB_PCT_RANK"],
+    },
+    {
+      label: "Defensive Rebound Percentage",
+      value: metrics["E_DREB_PCT"],
+      rank: metrics["E_DREB_PCT_RANK"],
+    },
+    {
+      label: "Rebound Percentage",
+      value: metrics["E_REB_PCT"],
+      rank: metrics["E_REB_PCT_RANK"],
+    },
+    {
+      label: "Turnover Percentage",
+      value: metrics["E_TOV_PCT"],
+      rank: metrics["E_TOV_PCT_RANK"],
+    },
+    {
+      label: "Usage Percentage",
+      value: metrics["E_USG_PCT"],
+      rank: metrics["E_USG_PCT_RANK"],
+    },
+    { label: "Pace", value: metrics["E_PACE"], rank: metrics["E_PACE_RANK"] },
+    { label: "Games Played", value: metrics["GP"], rank: metrics["GP_RANK"] },
+    { label: "Wins", value: metrics["W"], rank: metrics["W_RANK"] },
+    { label: "Losses", value: metrics["L"], rank: metrics["L_RANK"] },
+    {
+      label: "Win Percentage",
+      value: metrics["W_PCT"],
+      rank: metrics["W_PCT_RANK"],
+    },
   ];
 
   return (
@@ -50,25 +64,11 @@ const PlayerEstimatedMetrics = ({ metrics }) => {
       <ul>
         {playerMetricsLabels.map((metric, index) => (
           <li key={index}>
-            <strong>{metric.label}:</strong> {metric.value}
+            <strong>{metric.label}:</strong> {metric.value}{" "}
+            {metric.rank && `[#${metric.rank}]`}
           </li>
         ))}
       </ul>
-      <button
-        onClick={() => setShowAdvanced(!showAdvanced)}
-        className="bg-blue-500 text-white px-4 py-2 rounded mt-4"
-      >
-        {showAdvanced ? "Hide Advanced Stats" : "Show Advanced Stats"}
-      </button>
-      {showAdvanced && (
-        <ul className="mt-4">
-          {advancedMetricsLabels.map((metric, index) => (
-            <li key={index}>
-              <strong>{metric.label}:</strong> {metric.value}
-            </li>
-          ))}
-        </ul>
-      )}
     </div>
   );
 };
